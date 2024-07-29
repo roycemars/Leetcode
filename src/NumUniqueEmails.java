bf86ed91831d55221c5996b1ca71c8ebc2b76888
Royce Mars <roycemars@yahoo.com>
2024-07-28 23:14:56 -0700

    Number of unique emails: Split and replace approach added

--- src/NumUniqueEmails.java
+++ src/NumUniqueEmails.java
@@ -13,7 +13,46 @@ public class NumUniqueEmails {
                 "test.email+alex@leetcode.com",
                 "test.e.mail+bob.cathy@leetcode.com",
                 "testemail+david@lee.tcode.com"};
-        System.out.printf("\nNumber of unique emails: %d\n", linearIteration(emails));
+
+//        System.out.printf("\nNumber of unique emails: %d\n", linearIteration(emails));
+        System.out.printf("\nNumber of unique emails: %d\n", splitAndReplace(emails));
+    }
+
+    /**
+     * Same complexity but better readability
+     *
+     * Let N be the number of the emails and M be the average length of an email.
+     *
+     * Time Complexity: O(N⋅M)
+     * The split method must iterate over all of the characters in each email and the replace method must iterate over all of the characters in each local name. As such, they both require linear time and are O(M) operations.
+     * Since there are N emails and the average email has M characters in it, the complexity is of order (Number of emails) * (Number of characters in an email) = N*M.
+     *
+     * Space Complexity: O(N⋅M)
+     * In the worst case, when all emails are unique, we will store every email address given to us in the hash set.
+     * @param emails - array of input emails
+     * @return number of unique recipients
+     */
+    private static int splitAndReplace(String[] emails) {
+        // has set to store all the unique emails
+        Set<String> uniqueEmails = new HashSet<>();
+
+        // this output is for debug purposes only
+        System.out.println("Split and replace built-ins approach:");
+
+        // iterate over all emails
+        for (String email : emails) {
+            // split into two parts local and domain
+            String[] parts = email.split("@");
+
+            // split local by '+'
+            String[] local = parts[0].split("\\+");
+
+            String cleanMail = local[0].replace(".", "") + "@" + parts[1];
+            System.out.printf("%s -> %s\n", email, cleanMail);
+            uniqueEmails.add(cleanMail);
+        }
+
+        return uniqueEmails.size();
     }
 
     /**
@@ -38,7 +77,7 @@ public class NumUniqueEmails {
         Set<String> uniqueEmails = new HashSet<>();
 
         // this output is for debug purposes only
-        System.out.println("Logs:");
+        System.out.println("Linear iteration approach:");
 
         // iterate over all emails
         for (String email : emails) {
